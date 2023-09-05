431e7ec6a15409d9373302f7d824266d60282545
Kaspars <kasparsj@gmail.com>
2023-09-05 15:54:08 +0300

    small fix

--- src/format-arguments.js
+++ src/format-arguments.js
@@ -89,7 +89,7 @@ export default function formatArguments(transform, startIndex, synthContext) {
         typedArg.value = ensure_decimal_dot(typedArg.value)
       } else if (typedArg.type.startsWith('vec') && typeof typedArg.value !== 'function' && !typedArg.value.getTexture) {
         typedArg.isUniform = false
-        if (Array.isArray(typedArg.value)) {
+        if (Array.isArray(typedArg.value) || typedArg.value instanceof Float32Array || typedArg.value instanceof Uint8Array) {
           typedArg.value = `${typedArg.type}(${typedArg.value.map(ensure_decimal_dot).join(', ')})`
         }
         else if (typeof typedArg.value === 'number') {
