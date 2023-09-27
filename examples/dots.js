98852424db23004696ccd9f65e09aafdb6adb731
Kaspars <kasparsj@gmail.com>
2023-09-27 11:35:15 +0300

    fix dots example

--- examples/dots.js
+++ examples/dots.js
@@ -1,14 +1,13 @@
 await loadScript("/hy/index.js");
 await loadScript("/dt/index.js");
-await loadScript("/extra-shaders-for-hydra/lib-noise.js");
 
 // noise
-dots([50,50], solid(noise(0.4, 0.03).map(-1,1,-0.1,1.1), noise(0.6, 0.03).map(-1,1,-0.1,1.1)), noise(0.4).mult(10), colornoise(1000))
+dots([50,50], solid(noise(0.4, 0.03).map(-1,1,-0.1,1.1), noise(0.6, 0.03).map(-1,1,-0.1,1.1)), noise(0.4).mult(10), cnoise(1000))
     .fade(0.05)
     .out()
 
 // smearing color
-dots([500, 500], solid(pnoise(0.4, 0.1).map(-1,1,-0.1,1.1), noise(0.6, 0.03).map(-1,1,-0.1,1.1)), noise(0.4).mult(20), colornoise(100).saturate(()=>time%2.0))
+dots([500, 500], solid(pnoise(0.4, 0.1).map(-1,1,-0.1,1.1), noise(0.6, 0.03).map(-1,1,-0.1,1.1)), noise(0.4).mult(20), cnoise(100).saturate(()=>time%2.0))
     .fade(0.001)
     .out()
 
@@ -19,7 +18,7 @@ dots([800, 800], solid(noise(1, 0.01).map(-1,1,-0.2,1.2).scrollX(0, 0.01), noise
 // storm
 solid(0)
     .layer(
-        dots([20, 20], solid(noise(100).map(-1,1,0,1), tex(arr.random(50, 50, {max: 255}))), noise().mult(10), colornoise(1000, 0.1)
+        dots([20, 20], solid(noise(100).map(-1,1,0,1), tex(arr.random(50, 50, {max: 255}))), noise().mult(10), cnoise(1000, 0.1)
             .brightness())
             .autoClear()
     )
@@ -27,10 +26,15 @@ solid(0)
     .out();
 
 // circle
-dots([100,1], solid(sin().add(0.5), cos().add(0.5)), 3, colornoise(1000).rotate(0, 1).saturate(8)).out();
+dots([100,1], solid(sin().add(0.5), cos().add(0.5)), 3, cnoise(1000).rotate(0, 1).saturate(8)).out();
 
 // japan flag
-solid(1, 1, 1).layer(dots([1], 0.5, 500, solid(1))).out()
+solid(1, 1, 1, 1).layer(dots([1], 0.5, 500, solid(1))).out()
 
 // grid
 dots([10,10], gradient(), 10).out()
+
+// noise flow
+solid(0,1,0).layer(
+    dots([300,300], gradient().add(noise().mult(0.1)), wnoise().map(0,1,5,10), solid(0, 0, 0, 0.9))
+).out();
