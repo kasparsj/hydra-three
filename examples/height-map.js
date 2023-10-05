ce96f1f44cfefa342bfe9ae61bf62e3124cbb5da
Kaspars <kasparsj@gmail.com>
2023-10-06 00:10:14 +0300

    added height-map example

---
+++ examples/height-map.js
@@ -0,0 +1,31 @@
+await loadScript("/hy/index.js");
+
+shadowMap();
+o0.ortho([0,5,3], 0, {controls: true})
+
+canvas.setLinear()
+
+const tex = scene({background: color(1,1,1)})
+    .points([500, 500], mt.dots(solid(snoise(10), snoise(20)).map(-1,1,0,1), 2, [0,0,1]))
+    .tex(o1);
+
+// const tex = solid(1, 1, 1).layer(lines(0.003, 0.001, solid(0,0,1))).tex(o1, {mag: 'liōear', min: 'mipmap', width: 4096, height: 4096});
+
+const hMap = fbm(0.7, [0.5,-2]).tex(o2);
+
+const sc = scene()
+    .lights({all: true, gui: false})
+    .world()
+    .mesh(
+        gm.plane(2,2,250,250).rotateX(-Math.PI/2),
+        mt.meshPhong({map: tex, displacementMap: hMap, displacementScale: 2}),
+    ).out();
+
+update = () => {
+//     sc.at(0).rotation.z += 0.001;
+}
+
+// todo: why cannot increase plane resolution?
+// todo: shadows
+// todo: generate fbm in a more controlled way
+// todo: background
