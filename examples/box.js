97ca46a2ff70a13d8043ee7efa75a2625968ba0e
Kaspars Jaudzems <kasparsj@gmail.com>
2024-02-23 11:12:51 +0200

    update README

--- examples/box.js
+++ examples/box.js
@@ -1,11 +1,17 @@
 perspective([2,2,3], [0,0,0], {controls: true});
 
-const sc = scene().lights()
-    .mesh(gm.box(), osc().rotate(snoise(1).mult(45)).phong())
+// create geometry and material
+const geom = gm.box(); // cube geometry
+const mat = osc().rotate(noise(1).mult(45)); // use a hydra texture as material
+
+// compose scene
+const sc = scene()
+    .lights() // default lighting setup
+    .mesh(geom, mat.phong()) // add mesh to scene
     .out();
 
 update = () => {
     const box = sc.at(0);
-//     box.rotation.x += 0.01;
+    box.rotation.x += 0.01;
     box.rotation.y += 0.01;
 }
