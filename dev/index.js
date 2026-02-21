31c947564ae50c113b870911736663c06f3bee0d
Kaspars <kasparsj@gmail.com>
2026-02-21 15:15:55 +0200

    update dev example

--- dev/index.js
+++ dev/index.js
@@ -1,8 +1,25 @@
 const Hydra = require('./../src/index.js')
 
 function init() {
-  window.hydra = new Hydra({ detectAudio: false, makeGlobal: true })
-  osc(8, 0.1, 0.8).out()
+  window.hydraSynth = new Hydra({ detectAudio: false, makeGlobal: true })
+
+  perspective([2,2,3], [0,0,0], {controls: true});
+
+  // create geometry and material
+  const geom = gm.box();
+  const mat = osc().rotate(noise(1).mult(45)).phong();
+
+  // compose scene
+  const sc = scene()
+      .lights()
+      .mesh(geom, mat)
+      .out();
+
+  update = () => {
+    const box = sc.at(0);
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+  }
 }
 
 window.onload = init
